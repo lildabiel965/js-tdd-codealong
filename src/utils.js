3479f6e83c5af31d0179f04bcff0e064b4299a65
Daniel <daniel.mutembei@student.moringaschool.com>
2025-01-28 14:46:06 +0300

    done

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
 // Your code here
+import { currentAgeForBirthYear } from "../utils";
+
+describe("currentAgeForBirthYear", () => {
+    it("returns the age of a person based on the year of birth", () => {
+      const currentYear = new Date().getFullYear();
+      const birthYear = 1984;
+      const expectedAge = currentYear - birthYear;
+      const ageOfPerson = currentAgeForBirthYear(birthYear);
+      expect(ageOfPerson).toEqual(38);
+    });
+  });
